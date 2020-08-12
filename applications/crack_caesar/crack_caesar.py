c23aa8b55acfba9f4e8e764dce16818bb9665281
Kenneth Boelter <kennethboelter@pop-os.localdomain>
2020-08-11 20:16:12 -0600

    finished cipher

--- applications/crack_caesar/crack_caesar.py
+++ applications/crack_caesar/crack_caesar.py
@@ -2,5 +2,54 @@
 # decode it.
 
 # Your code here
+def convert_to_text(file):
+    with open(file, "r") as file:
+        data = file.read()
+        file.close()
+    return data
+
+text = convert_to_text("ciphertext.txt")
+
+def frequency(text):
+    freq = {}
+    total = 0
+    for char in text:
+        if char.isspace():
+            continue
+        elif not char.isalpha():
+            continue
+        elif char in freq:
+            total += 1
+            freq[char] += 1
+        else:
+            total += 1
+            freq[char] = 1
+
+    for letter, count in freq.items():
+        freq[letter] = round(count/total * 100, 2)
+
+    return {key: value for key, value in sorted(freq.items(), key=lambda item: item[1], reverse=True)}
+
+
+
 def cipher_text():
-    pass
+    letters = ['E', 'T', 'A', 'O', 'H', 'N', 'R', 'I', 'S', 'D', 'L', 'W', 'U', 'G', 'F', 'B', 'M', 'Y', 'C', 'P', 'K', 'V', 'Q', 'J', 'X', 'Z']
+    freq = frequency(text)
+
+    count = 0
+    for letter, perc in freq.items():
+        freq[letter] = letters[count]
+        count += 1
+
+    plain_text = ""
+    for char in convert_to_text("ciphertext.txt"):
+        if char.isspace():
+            plain_text += " "
+        elif not char.isalpha():
+            plain_text += char
+        else:
+            plain_text += freq[char]
+
+    print(plain_text)
+
+cipher_text()
